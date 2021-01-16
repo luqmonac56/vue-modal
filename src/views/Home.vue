6b9f672c0327c64ec60c3b301325e7422289d948
luqmonac56 <luqmonac56@gmail.com>
2021-01-16 20:39:22 +0100

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="toggleModal = !toggleModal" class="px-6 py-3 font-sans font-bold text-white bg-red-400 rounded shadow-lg outline-none active:outline-none focus:outline-none hover:bg-red-300 ">click here</button>
+    
+    <div class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto" v-if="toggleModal">
+      <div class="relative w-auto max-w-2xl mx-auto ">
+        <div class="flex flex-col w-full bg-white">
+        <div class="text-2xl font-bold"><h3 class="capitalize">Hello from vue modal</h3></div>
+          <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam, ducimus distinctio hic magni excepturi repellat voluptate odit laborum molestiae non ipsam nihil perferendis quas perspiciatis alias, dolorem repudiandae explicabo minima blanditiis quod nisi natus officia illum! Natus, doloribus excepturi pariatur itaque, maiores ab ipsa incidunt distinctio mollitia libero repellat!</span>
+         <button class="w-3/12 px-6 py-2 m-auto mt-1 mb-3 text-white bg-green-500 rounded shadow-2xl" @click="toggleModal = false"> close</button>
+          
+        </div>
+      </div>
+    </div>
+            
+    
+    <div v-if="toggleModal" class="absolute inset-0 z-30 bg-black opacity-25" >    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      toggleModal: false
+    };
+  },
 }
 </script>
